fd40cdba0a3f30eca891724bad34e01f284e0628
xray-robot <robot@xray.app>
2025-10-22 15:43:37 +0300

    Home page

--- app/components/pages/Home/index.tsx
+++ app/components/pages/Home/index.tsx
@@ -1,19 +1,25 @@
-import { SVGXray } from "@/svg"
-import style from "./style.module.css"
 import { useAppStore } from "@/store/app"
-import { NavLink } from "react-router"
-import Copy from "@/components/common/Copy"
-import { DocumentDuplicateIcon } from "@heroicons/react/24/outline"
+import { Button } from "antd"
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline"
 
 export default function HomePage() {
   const network = useAppStore((state) => state.network)
 
   return (
     <div className="text-center">
-      [home]
+      <div className="text-center mt-20">
+        <img src="/connected.png" alt="Connected" className="mx-auto mb-5 w-80 max-w-[80%]" />
+        <h1 className="text-5xl font-black mb-10">Connected!</h1>
+        <a href="https://xray.cool" target="_blank" rel="noreferrer">
+          <Button type="primary" shape="round" size="large" className="!px-10">
+            <span>Docs</span>
+            <ArrowUpRightIcon className="size-5" strokeWidth={2} />
+          </Button>
+        </a>
+      </div>
       <div className="text-center mt-10 text-sm text-gray-500">
-        <p>Mini Apps SDK: not connected</p>
-        <p>SDK Version: v1.0.0, Network: {network || ""}</p>
+        <p>SDK Version: —</p>
+        <p>Network: {network || ""}</p>
       </div>
     </div>
   )
